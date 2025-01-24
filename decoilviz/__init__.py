958f35ebc21c8daeb34ee45be100fb2a39ac83b4
madagiurgiu25 <giurgiumadalina25@gmail.com>
2025-01-24 08:36:24 +0100

    version 1.0.4

--- decoilviz/__init__.py
+++ decoilviz/__init__.py
@@ -1,7 +1,7 @@
 """
 Decoil-viz - visualize ecDNA reconstruction threads
 """
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__ = "Madalina Giurgiu"
 __credit__ = "Charite Universitaetsmedizin Berlin and Freie Universitaet Berlin"
 _program = "decoil-viz"
